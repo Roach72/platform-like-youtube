20a12bf2642e7e5c22b9e6d256f22ef8898dbafd
Roach72 <103392055+Roach72@users.noreply.github.com>
2022-07-01 14:32:13 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(".first-list-element").addClass("active")
+    
+    $(".nav-item").click(function(){
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+
+    });
+    
+    $("#toggler").click(function(event){
+        $('#wrap').toggleClass('toggled');
+
+        var right = $('.sidebar').css("right");
+        if (right == '0px')
+        {
+            $('.sidebar').css({'right': '-17rem' });
+            $('.layer').fadeOut();
+        }
+        else{
+            $('.sidebar').css({ 'right': '0' });
+            $('.layer').fadeIn();
+        }
+    });
+        
+        $(".search-icon").click(function() {
+            $(".search-input").slideToggle("slow");
+        });
+   });
+    
